742fa5819d569e3329ca5827d8dd444a9006eba6
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 18:50:12 +0700

    add generate

--- blocks/blocks_7-color-flash.js
+++ blocks/blocks_7-color-flash.js
@@ -1,15 +1,19 @@
-Blockly.Blocks['_7_color_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("7 COLOR LED");
-    this.appendValueInput("7_COLOR_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["_7_color_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("7 COLOR LED");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "7_COLOR_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("7_COLOR_STATE"),
+          "7_COLOR_STATE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_7-color-flash.js
+++ blocks/generators_7-color-flash.js
@@ -1,6 +1,26 @@
-Blockly.JavaScript['_7_color_block'] = function(block) {
-  var value_7_color_digital_pin = Blockly.JavaScript.valueToCode(block, '7_COLOR_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["_7_color_block"] = function(block) {
+    var text_7_color_pin = block.getFieldValue("7_COLOR_PIN");
+    var variable_7_color_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("7_COLOR_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+	int V_7_COLOR_PIN =  ${text_7_color_pin};  // control coonected to digital pin 11
+	#END
+
+	// The setup() method runs once, when the sketch starts
+
+	#SETUP           
+	  // initialize the digital pin as an output:
+	  pinMode(V_7_COLOR_PIN, OUTPUT);
+	  digitalWrite(V_7_COLOR_PIN,LOW);  //ser the crtl off
+	#END
+	// the loop() method runs over and over again,
+	// as long as the Arduino has power
+
+	${variable_7_color_state} = V_7_COLOR_PIN;
+    `;
+    return code;
+  };
